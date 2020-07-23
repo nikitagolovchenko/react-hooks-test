3c7ab3a238994949c5523ad4c24c4c3bb10fdbaa
Nikita Golovchenko <sunburst1715@gmail.com>
2020-07-23 13:58:50 +0300

    Добавил главную страницу

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="container pt-4">
-      <h1>Hello World</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <div className='container pt-4'>
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/about' component={About} />
+          <Route path='/profile/:name' component={Profile} />
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
